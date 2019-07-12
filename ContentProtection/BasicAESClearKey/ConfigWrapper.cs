94c65774c2c4e6c24808610c3f3e3957c47487ae
Jinlin Zhang (Hi-Tech Talents LLC) <v-jizhang@microsoft.com>
2019-07-12 18:28:50 +0100

    Check in BasicAESClearKey

---
+++ ContentProtection/BasicAESClearKey/ConfigWrapper.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+using Microsoft.Extensions.Configuration;
+
+namespace BasicAESClearKey
+{
+    public class ConfigWrapper
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigWrapper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string SubscriptionId
+        {
+            get { return _config["SubscriptionId"]; }
+        }
+
+        public string ResourceGroup
+        {
+            get { return _config["ResourceGroup"]; }
+        }
+
+        public string AccountName
+        {
+            get { return _config["AccountName"]; }
+        }
+
+        public string AadTenantId
+        {
+            get { return _config["AadTenantId"]; }
+        }
+
+        public string AadClientId
+        {
+            get { return _config["AadClientId"]; }
+        }
+
+        public string AadSecret
+        {
+            get { return _config["AadSecret"]; }
+        }
+
+        public Uri ArmAadAudience
+        {
+            get { return new Uri(_config["ArmAadAudience"]); }
+        }
+
+        public Uri AadEndpoint
+        {
+            get { return new Uri(_config["AadEndpoint"]); }
+        }
+
+        public Uri ArmEndpoint
+        {
+            get { return new Uri(_config["ArmEndpoint"]); }
+        }
+
+        public string Region
+        {
+            get { return _config["Region"]; }
+        }
+
+        public string EventHubConnectionString
+        {
+            get { return _config["EventHubConnectionString"]; }
+        }
+
+        public string EventHubName
+        {
+            get { return _config["EventHubName"]; }
+        }
+
+        public string StorageContainerName
+        {
+            get { return _config["StorageContainerName"]; }
+        }
+
+        public string StorageAccountName
+        {
+            get { return _config["StorageAccountName"]; }
+        }
+
+        public string StorageAccountKey
+        {
+            get { return _config["StorageAccountKey"]; }
+        }
+    }
+}
