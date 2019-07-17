46168480ff183e54ac963723ce6b9566268952e8
Jinlin Zhang (Hi-Tech Talents LLC) <v-jizhang@microsoft.com>
2019-07-18 00:03:22 +0100

    Added comments

--- ContentProtection/BasicPlayReady/ConfigWrapper.cs
+++ ContentProtection/BasicPlayReady/ConfigWrapper.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 
 using Microsoft.Extensions.Configuration;
 
 namespace BasicPlayReady
 {
+    /// <summary>
+    /// This class reads values from local configuration file appsettings.json
+    /// Please change the configuration using your account information. For more information, see
+    /// https://docs.microsoft.com/azure/media-services/latest/access-api-cli-how-to. For security
+    /// reasons, do not check in the configuration file to source control.
+    /// </summary>
     public class ConfigWrapper
     {
         private readonly IConfiguration _config;
