25ea7fff939a1097f979cc40aa263847e694fe91
Simas Petrikis <sipetrik@microsoft.com>
2020-05-07 13:37:48 -0400

    Updated AMS client to use service identity for auth

--- EncodingStreamingHighAvailability/media-services-high-availability-shared/Helpers/MediaServicesHelper.cs
+++ EncodingStreamingHighAvailability/media-services-high-availability-shared/Helpers/MediaServicesHelper.cs
@@ -5,6 +5,7 @@ namespace media_services_high_availability_shared.Helpers
     using media_services_high_availability_shared.Models;
     using Microsoft.Azure.Management.Media;
     using Microsoft.Azure.Management.Media.Models;
+    using Microsoft.Azure.Services.AppAuthentication;
     using Microsoft.IdentityModel.Clients.ActiveDirectory;
     using Microsoft.Rest;
     using Microsoft.Rest.Azure.Authentication;
@@ -27,30 +28,16 @@ namespace media_services_high_availability_shared.Helpers
                 throw new ArgumentNullException(nameof(config));
             }
 
-            var credentials = await GetCredentialsAsync(config).ConfigureAwait(false);
+            // Authenticate to Azure.
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com").ConfigureAwait(false);
+            ServiceClientCredentials credentials = new TokenCredentials(accessToken);
 
-            return new AzureMediaServicesClient(config.ArmEndpoint, credentials)
+            // Establish a connection to Media Services.
+            return new AzureMediaServicesClient(credentials)
             {
-                SubscriptionId = config.SubscriptionId,
+                SubscriptionId = config.SubscriptionId
             };
-        }
-
-        /// <summary>
-        /// Create the ServiceClientCredentials object based on the credentials
-        /// supplied in local configuration file.
-        /// </summary>
-        /// <param name="config">The parm is of type ConfigWrapper. This class reads values from local configuration file.</param>
-        /// <returns></returns>
-        // <GetCredentialsAsync>
-        public static async Task<ServiceClientCredentials> GetCredentialsAsync(MediaServiceConfigurationModel config)
-        {
-            if (config == null)
-            {
-                throw new ArgumentNullException(nameof(config));
-            }
-
-            var clientCredential = new ClientCredential(config.AadClientId, config.AadSecret);
-            return await ApplicationTokenProvider.LoginSilentAsync(config.AadTenantId, clientCredential, ActiveDirectoryServiceSettings.Azure).ConfigureAwait(false);
         }
 
         public static async Task<Transform> EnsureTransformExists(IAzureMediaServicesClient client, string resourceGroupName, string accountName, string transformName, Preset preset)
