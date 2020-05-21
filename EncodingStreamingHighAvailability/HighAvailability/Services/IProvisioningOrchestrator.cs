fba8d9bdacea087c90c080d4f6f0991f950c8fa4
Simas Petrikis <sipetrik@microsoft.com>
2020-05-21 15:54:30 -0400

    Introduced provisioningOrchestrator

---
+++ EncodingStreamingHighAvailability/HighAvailability/Services/IProvisioningOrchestrator.cs
@@ -0,0 +1,11 @@
+﻿namespace HighAvailability.Services
+{
+    using HighAvailability.Models;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
+
+    public interface IProvisioningOrchestrator
+    {
+        Task ProvisionAsync(StreamProvisioningRequestModel request, ILogger logger);
+    }
+}
---
+++ EncodingStreamingHighAvailability/HighAvailability/Services/ProvisioningOrchestrator.cs
@@ -0,0 +1,31 @@
+﻿namespace HighAvailability.Services
+{
+    using HighAvailability.Helpers;
+    using HighAvailability.Models;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class ProvisioningOrchestrator : IProvisioningOrchestrator
+    {
+        private readonly IList<IProvisioningService> provisioningServices;
+
+        public ProvisioningOrchestrator(IList<IProvisioningService> provisioningServices)
+        {
+            this.provisioningServices = provisioningServices ?? throw new ArgumentNullException(nameof(provisioningServices));
+        }
+
+        public async Task ProvisionAsync(StreamProvisioningRequestModel request, ILogger logger)
+        {
+            logger.LogInformation($"ProvisioningOrchestrator::ProvisionAsync started: streamProvisioningRequest={LogHelper.FormatObjectForLog(request)}");
+
+            foreach (var service in this.provisioningServices)
+            {
+                await service.ProvisionAsync(request, logger).ConfigureAwait(false);
+            }
+
+            logger.LogInformation($"ProvisioningOrchestrator::ProvisionAsync completed: streamProvisioningRequest={LogHelper.FormatObjectForLog(request)}");
+        }
+    }
+}
