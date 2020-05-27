87c995369fd7366a6606195e71088ccc0bc2e3dd
Simas Petrikis <sipetrik@microsoft.com>
2020-05-27 10:10:51 -0400

    more comments

--- EncodingStreamingHighAvailability/HighAvailability/AzureStorage/Models/JobOutputStatusModelTableEntity.cs
+++ EncodingStreamingHighAvailability/HighAvailability/AzureStorage/Models/JobOutputStatusModelTableEntity.cs
@@ -4,12 +4,22 @@
     using Microsoft.Azure.Cosmos.Table;
     using System;
 
+    /// <summary>
+    /// Implements table storage specific model for JobOutputStatusModel class
+    /// </summary>
     public class JobOutputStatusModelTableEntity : TableEntity
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public JobOutputStatusModelTableEntity() : this(new JobOutputStatusModel())
         {
         }
 
+        /// <summary>
+        /// Constructor to create object from JobOutputStatusModel object
+        /// </summary>
+        /// <param name="jobOutputStatusModel">source object</param>
         public JobOutputStatusModelTableEntity(JobOutputStatusModel jobOutputStatusModel)
         {
             this.PartitionKey = jobOutputStatusModel.JobName;
@@ -25,18 +35,49 @@
         }
 
         /// <summary>
-        /// TBD need to decide if we want to duplicate PartitionKey and RowKey fields in table storage, for now, we are duplicating
-        /// We can not use Id field for cosmos db, changed name to JobOutputStatusId
+        /// Unique Id
         /// </summary>
         public string JobOutputStatusId { get; set; }
+
+        /// <summary>
+        /// Encoding job name
+        /// </summary>
         public string JobName { get; set; }
+
+        /// <summary>
+        /// Encoding job output asset name
+        /// </summary>
         public string JobOutputAssetName { get; set; }
+
+        /// <summary>
+        /// Encoding job output state
+        /// </summary>
         public string JobOutputState { get; set; }
+
+        /// <summary>
+        /// Event DateTimeOffset
+        /// </summary>
         public DateTimeOffset EventTime { get; set; }
+
+        /// <summary>
+        /// Azure Media Services instance account name
+        /// </summary>
         public string MediaServiceAccountName { get; set; }
+
+        /// <summary>
+        /// Encoding job transform name
+        /// </summary>
         public string TransformName { get; set; }
+
+        /// <summary>
+        /// If encoding job output failed, this indicates if it can be successfully resubmitted
+        /// </summary>
         public bool IsSystemError { get; set; }
 
+        /// <summary>
+        /// Creates JobOutputStatusModel object
+        /// </summary>
+        /// <returns>JobOutputStatusModel object</returns>
         public JobOutputStatusModel GetJobOutputStatusModel()
         {
             return new JobOutputStatusModel
--- EncodingStreamingHighAvailability/HighAvailability/Models/ProvisioningRequestModel.cs
+++ EncodingStreamingHighAvailability/HighAvailability/Models/ProvisioningRequestModel.cs
@@ -1,10 +1,28 @@
 ﻿namespace HighAvailability.Models
 {
+    /// <summary>
+    /// Implements data model to store provisioning request data
+    /// </summary>
     public class ProvisioningRequestModel
     {
+        /// <summary>
+        /// Unique id
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// Encoding job output asset name
+        /// </summary>
         public string EncodedAssetName { get; set; }
+
+        /// <summary>
+        /// Azure Media Services instance account name associated with encoding job
+        /// </summary>
         public string EncodedAssetMediaServiceAccountName { get; set; }
+
+        /// <summary>
+        /// Streaming locator name to stream encoded asset data
+        /// </summary>
         public string StreamingLocatorName { get; set; }
     }
 }
