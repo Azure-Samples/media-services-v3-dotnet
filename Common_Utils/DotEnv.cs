5b1661880f358f17bfeb8eed60bc54d8794d3bfb
xpouyat@microsoft.com <xpouyat@microsoft.com>
2021-05-19 17:02:38 +0200

    Centralize DotEnv.cs in the Common-Utils library.

--- Common_Utils/DotEnv.cs
+++ Common_Utils/DotEnv.cs
@@ -4,7 +4,7 @@
 using System;
 using System.IO;
 
-namespace EncodingWithMESCustomHEVC
+namespace Common_Utils
 {
     /// <summary>
     /// This class is used to read the ".env" file if the IDE is Visual Studio. (VS Code has its own way to read it using launch.json)
