82117da8dd7805450da009026cef7331b8a8ae02
xpouyat@microsoft.com <xpouyat@microsoft.com>
2021-05-26 10:00:43 +0200

    Update to Widevine template.

---
+++ ContentProtection/BasicWidevine/WidevineConfig/ContentKeySpec.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace WidevineConfig
+{
+    /// <summary>
+    /// Widevine ContentKeySpec class.
+    /// </summary>
+    public class ContentKeySpec
+    {
+        /// <summary>
+        /// Gets or sets track type.
+        /// If content_key_specs is specified in the license request, make sure to specify all track types explicitly.
+        /// Failure to do so results in failure to play back past 10 seconds.
+        /// </summary>
+        [JsonProperty("track_type")]
+        public string TrackType { get; set; }
+
+        /// <summary>
+        /// Gets or sets client robustness requirements for playback.
+        /// Software-based white-box cryptography is required.
+        /// Software cryptography and an obfuscated decoder are required.
+        /// The key material and cryptography operations must be performed within a hardware-backed trusted execution environment.
+        /// The cryptography and decoding of content must be performed within a hardware-backed trusted execution environment.
+        /// The cryptography, decoding, and all handling of the media (compressed and uncompressed) must be handled within a hardware-backed trusted execution environment.
+        /// </summary>
+        [JsonProperty("security_level")]
+        public int SecurityLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OutputProtection.
+        /// </summary>
+        [JsonProperty("required_output_protection")]
+        public OutputProtection RequiredOutputProtection { get; set; }
+    }
+}
---
+++ ContentProtection/BasicWidevine/WidevineConfig/OutputProtection.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace WidevineConfig
+{
+    /// <summary>
+    /// OutputProtection Widevine class.
+    /// </summary>
+    public class OutputProtection
+    {
+        /// <summary>
+        /// Gets or sets HDCP protection.
+        /// Supported values : HDCP_NONE, HDCP_V1, HDCP_V2
+        /// </summary>
+        [JsonProperty("hdcp")]
+        public string HDCP { get; set; }
+
+        /// <summary>
+        /// Gets or sets CGMS.
+        /// </summary>
+        [JsonProperty("cgms_flags")]
+        public string CgmsFlags { get; set; }
+    }
+}
