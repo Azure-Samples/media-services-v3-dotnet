2b16c4198076cd49492e9382ffd7dadcda8bd758
xpouyat@microsoft.com <xpouyat@microsoft.com>
2021-08-30 14:13:29 +0200

    Fix an issue when there is '=" in a connection string

--- Common_Utils/Configuration/DotEnv.cs
+++ Common_Utils/Configuration/DotEnv.cs
@@ -36,6 +36,7 @@ namespace Common_Utils
 
                 var parts = line.Split(
                     '=',
+                    2,
                     StringSplitOptions.RemoveEmptyEntries);
 
                 if (parts.Length != 2)
